ff3be0e66a1e562b828560fffd728bdc70f057f8
kunchalaravinder93 <kunchalaravinder93@gmail.com>
2026-03-25 16:34:56 +0530

    fix pipeline errors

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 echo "Starting Security + Performance Pipeline"
@@ -7,10 +6,7 @@ echo "Starting Security + Performance Pipeline"
 echo "Running JMeter Load Test..."
 jmeter -n -t jmeter/test-plan.jmx -l result.jtl
 
-echo "Generating JMeter HTML Report..."
-jmeter -g result.jtl -o jmeter-report
-
 echo "Running OWASP ZAP Security Scan..."
-docker run --rm -v $(pwd):/zap/wrk/:rw owasp/zap2docker-stable zap-baseline.py -t https://jsonplaceholder.typicode.com -r zap-report.html
+docker run --rm -v $(pwd):/zap/wrk ghcr.io/zaproxy/zaproxy:stable zap-baseline.py -t https://jsonplaceholder.typicode.com -r zap-report.html
 
-echo "Pipeline finished successfully"
+echo "Pipeline completed"
